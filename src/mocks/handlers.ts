cf4ede6683cbd70c91acebe0d96c174c604dca91
Stefan Puia <stefan@stefanpuia.co.uk>
2022-04-05 21:54:36 +0100

    dependencies

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -8,7 +8,7 @@ const builds: any = {};
 export default [
   rest.get(`${baseURL}/build/:buildId`, (req, res, { delay, json, status }) =>
     {
-      let build = builds[req.params.buildId];
+      let build = builds[req.params.buildId as string];
       switch (req.params.buildId) {
         case "grouped0": build = BUILD_UNGROUPED; break;
         case "grouped1": build = BUILD_GROUPED; break;
