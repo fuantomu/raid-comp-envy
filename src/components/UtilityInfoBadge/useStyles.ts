41a4d7bae6f8968008978f80fb52c8cf5a66da8a
Stefan <stefan@stefanpuia.co.uk>
2022-08-29 15:18:51 +0100

    closes #194 Added warning message to buff and utility checks

---
+++ src/components/UtilityInfoBadge/useStyles.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+import useTheme, { Spacing } from "../../utils/useTheme";
+
+export default () => {
+  const theme = useTheme();
+  return {
+    icon: css`
+      position: absolute;
+      top: ${theme.spacing(Spacing.s)};
+      right: ${theme.spacing(Spacing.s)};
+    `,
+    container: css`
+      position: relative;
+    `,
+  };
+};
