c9c05306d9b0fbabe17c713aabdf3d64f33531d6
fuantomu <fuantomuw@gmail.com>
2023-11-17 20:15:59 +0100

    attempt to fix mysql connection

--- api/src/main/java/uk/raidcomp/api/data/entity/BuildEntity.java
+++ api/src/main/java/uk/raidcomp/api/data/entity/BuildEntity.java
@@ -1,18 +1,30 @@
 package uk.raidcomp.api.data.entity;
 
 import io.micronaut.data.annotation.DateCreated;
-import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import static jakarta.persistence.GenerationType.AUTO;
+
 import java.time.Instant;
 import java.util.List;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @MappedEntity("builds")
+@Entity
 public class BuildEntity {
-  @Id private String id;
+  @Id
+  @GeneratedValue(strategy = AUTO)
+  private String id;
 
   private String name;
 
@@ -20,5 +32,6 @@ public class BuildEntity {
 
   @DateCreated private long lastSeen;
 
+  @JdbcTypeCode(SqlTypes.JSON)
   private List<PlayerEntity> players;
 }
--- api/src/main/java/uk/raidcomp/api/data/entity/PlayerEntity.java
+++ api/src/main/java/uk/raidcomp/api/data/entity/PlayerEntity.java
@@ -8,14 +8,24 @@ import uk.raidcomp.api.model.InviteStatus;
 import uk.raidcomp.api.model.WarcraftPlayerClass;
 import uk.raidcomp.api.model.WarcraftPlayerSpec;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+
 @Getter
 @Setter
 @Serdeable
+@Entity
 public class PlayerEntity {
+  @Id
   private String name;
+
   private String realm;
+
   private WarcraftPlayerClass className;
+
   private WarcraftPlayerSpec spec;
+
   private InviteStatus status;
+
   private GroupId groupId;
 }
