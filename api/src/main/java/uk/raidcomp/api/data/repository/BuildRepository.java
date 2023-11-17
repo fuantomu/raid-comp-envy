631cf31d6a8e4502e4d5f09ab58091b5ccf44cfd
fuantomu <fuantomuw@gmail.com>
2023-11-17 16:47:56 +0100

    change azure db to jdbc

--- api/src/main/java/uk/raidcomp/api/data/repository/BuildRepository.java
+++ api/src/main/java/uk/raidcomp/api/data/repository/BuildRepository.java
@@ -1,11 +1,12 @@
 package uk.raidcomp.api.data.repository;
 
-import io.micronaut.data.cosmos.annotation.CosmosRepository;
+import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 import java.util.List;
 import uk.raidcomp.api.data.entity.BuildEntity;
 
-@CosmosRepository
+@JdbcRepository(dialect = Dialect.MYSQL)
 public interface BuildRepository extends CrudRepository<BuildEntity, String> {
   List<BuildEntity> findByLastSeenLessThanEquals(long lastSeenMaxEpoch);
 }
