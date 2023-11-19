6020b62146e62060ba5f127d1c7c54cf6d5e3fa2
Stefan <stefan@stefanpuia.co.uk>
2023-11-19 20:03:10 +0000

    fix: clicking edit button from versioned build should now open the edit page

--- ui/src/routes/[[gameVersion]]/build/[buildId]/edit/+page.ts
+++ ui/src/routes/[[gameVersion]]/build/[buildId]/edit/+page.ts
@@ -2,5 +2,5 @@ import type { PageLoad } from './$types';
 import { routeToCorrectBuildUrl } from '$lib/buildRouting';
 
 export const load: PageLoad = async ({ params }) => {
-	return await routeToCorrectBuildUrl(params);
+	return await routeToCorrectBuildUrl(params, true);
 };
