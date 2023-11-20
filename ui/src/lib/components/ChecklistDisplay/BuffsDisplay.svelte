b541d4e55f929e823de24cdf8f3e42e553a51bbb
Stefan <stefan@stefanpuia.co.uk>
2023-11-20 20:47:17 +0000

    added checklist warning

--- ui/src/lib/components/ChecklistDisplay/BuffsDisplay.svelte
+++ ui/src/lib/components/ChecklistDisplay/BuffsDisplay.svelte
@@ -1,33 +1,16 @@
 <script lang="ts">
-  import Paper, {Content} from '@smui/paper';
-  import {_} from "svelte-i18n";
+  import { _ } from "svelte-i18n";
   import ChecklistCounter from "$lib/components/ChecklistDisplay/ChecklistCounter.svelte";
-  import {build, context} from "$lib/store";
+  import { build, context } from "$lib/store";
+  import ChecklistWrapper from "$lib/components/ChecklistDisplay/ChecklistWrapper.svelte";
 </script>
 
-<Paper>
-    <Content>
-        <div class="header">
-            <span>{$_(`build.checklist.buffs`)}</span>
-        </div>
-
-        <div>
-            {#each $context.gameVersion.getBuffs() as raidBuff}
-                <ChecklistCounter
-                        label={$_(`buff.${raidBuff.slug}`)}
-                        count={$context.gameVersion.countBuffs($build.players, raidBuff)}
-                        iconSrc={$context.iconProvider.getSrc(raidBuff.icon)}
-                />
-            {/each}
-        </div>
-    </Content>
-</Paper>
-
-<style>
-    .header {
-        display: grid;
-        grid-template-columns: auto 1fr auto;
-        align-items: center;
-        gap: var(--spacing-xs);
-    }
-</style>
+<ChecklistWrapper title={$_(`build.checklist.buffs`)} warning>
+  {#each $context.gameVersion.getBuffs() as raidBuff}
+    <ChecklistCounter
+      label={$_(`buff.${raidBuff.slug}`)}
+      count={$context.gameVersion.countBuffs($build.players, raidBuff)}
+      iconSrc={$context.iconProvider.getSrc(raidBuff.icon)}
+    />
+  {/each}
+</ChecklistWrapper>
--- ui/src/lib/components/ChecklistDisplay/ClassDisplay.svelte
+++ ui/src/lib/components/ChecklistDisplay/ClassDisplay.svelte
@@ -1,34 +1,17 @@
 <script lang="ts">
-  import Paper, {Content} from '@smui/paper';
-  import {_} from "svelte-i18n";
+  import { _ } from "svelte-i18n";
   import ChecklistCounter from "$lib/components/ChecklistDisplay/ChecklistCounter.svelte";
-  import {build, context} from "$lib/store";
+  import { build, context } from "$lib/store";
+  import ChecklistWrapper from "$lib/components/ChecklistDisplay/ChecklistWrapper.svelte";
 </script>
 
-<Paper>
-    <Content>
-        <div class="header">
-            <span>{$_(`build.checklist.classes`)}</span>
-        </div>
-
-        <div>
-            {#each $context.gameVersion.getClasses() as playerClass}
-                <ChecklistCounter
-                        label={$_(`classes.${playerClass.slug}`)}
-                        count={$context.gameVersion.countClasses($build.players, playerClass)}
-                        iconSrc={$context.iconProvider.getSrc(playerClass.icon)}
-                        colour={playerClass.colour}
-                />
-            {/each}
-        </div>
-    </Content>
-</Paper>
-
-<style>
-    .header {
-        display: grid;
-        grid-template-columns: auto 1fr auto;
-        align-items: center;
-        gap: var(--spacing-xs);
-    }
-</style>
+<ChecklistWrapper title={$_(`build.checklist.classes`)}>
+  {#each $context.gameVersion.getClasses() as playerClass}
+    <ChecklistCounter
+      label={$_(`classes.${playerClass.slug}`)}
+      count={$context.gameVersion.countClasses($build.players, playerClass)}
+      iconSrc={$context.iconProvider.getSrc(playerClass.icon)}
+      colour={playerClass.colour}
+    />
+  {/each}
+</ChecklistWrapper>
