06b0c893c822d78bd52ff812ec4732a0fe022c9b
fuantomu <fuantomuw@gmail.com>
2023-11-22 14:55:48 +0100

    removed unneeded mock

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,6 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import Providers from "./components/Providers";
 
-if (process.env.REACT_APP_USE_MOCK === "true" && process.env.NODE_ENV === "development") {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires, global-require
-  const { worker } = require("./mocks/browser");
-
-  worker.start({
-    onUnhandledRequest: "bypass",
-    quiet: true,
-  });
-}
-
 const container = document.getElementById("app-root");
 const root = createRoot(container!);
 root.render(
