3ee30483a96067b12c8928d69621e439a888cfab
fuantomu <fuantomuw@gmail.com>
2024-01-21 19:47:53 +0100

    removed strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,13 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import Providers from "./components/Providers";
-import { StrictMode } from "react";
 
 const container = document.getElementById("app-root");
 const root = createRoot(container!);
 root.render(
-  <StrictMode>
   <Providers>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Providers>
-  </StrictMode>
 );
